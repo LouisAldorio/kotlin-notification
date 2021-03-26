75a008a0aed2d063e31b645737fa6f5ce199ca12
louisaldorio <louisaldorio@gmail.com>
2021-03-26 19:31:30 +0700

    first commit

---
+++ app/src/main/java/com/mindorks/framework/notifications/AfterNotification.kt
@@ -0,0 +1,11 @@
+package com.mindorks.framework.notifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AfterNotification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_notification)
+    }
+}
